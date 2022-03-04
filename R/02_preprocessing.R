5bbf67257102a04d18dd1e3c632c7faba5114fe9
Guy Hunt <guypwhunt1992@gmail.com>
2022-03-04 14:45:54 +0000

    Adding CSVs

--- R/02_preprocessing.R
+++ R/02_preprocessing.R
@@ -50,8 +50,6 @@ read.flow_csv <- function(pathIN){
 # Read all:
 dfs <- sapply(filenames,read.flow_csv,simplify=FALSE)
 gc()
-save.image()
-gc()
 
 ##############################
 #REWRITE TO FLOWFRAME/FLOWSET#
@@ -73,14 +71,12 @@ csv_2_ff <- function(dat){
 # rewrite to flowframe
 dfs_ff = sapply(dfs,function(x) csv_2_ff(x),simplify=FALSE)
 gc()
-save.image()
-gc()
+rm(dfs)
 
 # rewrite to flowset
 dfs_fs <- as(dfs_ff,"flowSet")
 gc()
-save.image()
-gc()
+rm(dfs_ff)
 
 
 ###############################
@@ -90,27 +86,53 @@ gc()
 ## Each parameter of interest needs to be arcsinh transformed with an individual cofactor. The cofactor can be deduced from the size of the linear region around zero on a biexponential scale, as plotted in a histogram (in conventional gating software).
 # Choose manual transformation or automated transformation (we prefer manual)
 # Define parameters and cofactors for transformations:
-manualcofactors <- c(FSC.A=14000,SSC.A=24500,GPR32...AF488.A=170,FoxP3.PE.A=430,CD19...PE.CF595.A=1350,IgD...PerCP.Cy5.5.A=2.75,IFNg.PE.Cy7.A=24,FPRL1...AF647.A=2650,Zombie.NIR.A=1390,IL.17...BV421.A=115,
-                     CD24...BV605.A=1900,CD27...BV650.A=6.5)
-dfs_fs_t_manual <- transFlowVS(dfs_fs, channels=names(manualcofactors), cofactor=manualcofactors)
-gc()
-save.image()
+#manualcofactors <- c(FSC.A=14000,SSC.A=24500,GPR32...AF488.A=170,FoxP3.PE.A=430,CD19...PE.CF595.A=1350,IgD...PerCP.Cy5.5.A=2.75,IFNg.PE.Cy7.A=24,FPRL1...AF647.A=2650,Zombie.NIR.A=1390,IL.17...BV421.A=115,
+#                     CD24...BV605.A=1900,CD27...BV650.A=6.5)
+#dfs_fs_t_manual <- transFlowVS(dfs_fs, channels=names(manualcofactors), cofactor=manualcofactors)
 gc()
 
 columnNames <- columnNames[c(13:(length(columnNames)-2))]
 gc()
 automatedcofactors <- estParamFlowVS(dfs_fs, columnNames) #this may take a while.
 gc()
-save.image()
-gc()
 
 #auto
 dfs_fs_t_auto <- transFlowVS(dfs_fs, channels=columnNames, cofactor=automatedcofactors)
 gc()
-save.image()
+rm(dfs_fs)
+rm(automatedcofactors)
+
+
+##############################
+######## NORMALIZATION #######
+##############################
+
+## To correct for technical inter-sample variation we apply normalization by fdaNorm (which automatically detects the number of peaks)
+# We continue with the manual transformed dataset
+# Select the markers which require normalization (based on the densityplots you generated above). Be aware that you don't remove biological variation!
+gc()
+dfs_fs_t_auto_normfda <- warpSet(dfs_fs_t_auto, stains=c('FSC.A','SSC.A','GPR32...AF488.A','FoxP3.PE.A','CD19...PE.CF595.A','IgD...PerCP.Cy5.5.A', 'FPRL1...AF647.A', 'Zombie.NIR.A', 'IL.17...BV421.A', 'CD24...BV605.A', 'CD27...BV650.A'))
 gc()
 
-#check whether your are satisfied with the transformed parameters
+##############################
+####### EXPORT TO FCS ########
+##############################
+
+## The flowset (dfs_fs_t_auto_normfda) can be exported to individual fcs files
+# Create an 'output' folder
+gc()
+dir.create("dataPPOutput", showWarnings = FALSE)
+gc()
+
+#Save flowframes wihtin flowset as fcs files using the flowCore package
+write.flowSet(dfs_fs_t_auto_normfda, outdir='dataPPOutput', filename = paste0(gsub(".csv", ".fcs", sampleNames(dfs_fs_t_auto_normfda))))
+gc()
+
+##############################
+########### Plots ############
+##############################
+
+# Pre-Normalized Plots
 flowViz.par.set(theme =  trellis.par.get(), reset = TRUE)
 
 dir.create("Figures", showWarnings = FALSE)
@@ -142,55 +164,29 @@ densityplot(~CD24...BV605.A+CD27...BV650.A, dfs_fs_t_auto, main="auto")
 dev.off()
 gc()
 
-##############################
-######## NORMALIZATION #######
-##############################
-
-## To correct for technical inter-sample variation we apply normalization by fdaNorm (which automatically detects the number of peaks)
-# We continue with the manual transformed dataset
-# Select the markers which require normalization (based on the densityplots you generated above). Be aware that you don't remove biological variation!
-gc()
-dfs_fs_t_manual_normfda <- warpSet(dfs_fs_t_auto, stains=c('FSC.A','SSC.A','GPR32...AF488.A','FoxP3.PE.A','CD19...PE.CF595.A','IgD...PerCP.Cy5.5.A', 'FPRL1...AF647.A', 'Zombie.NIR.A', 'IL.17...BV421.A', 'CD24...BV605.A', 'CD27...BV650.A'))
-gc()
-save.image()
-gc()
-
-#check whether you are satisfied
+# Normalized Plots
 gc()
 jpeg(file = paste0(figureDirectory,"normaliseddensityplot1.jpeg"))
-densityplot(~FSC.A+SSC.A, dfs_fs_t_manual_normfda, main="auto")
+densityplot(~FSC.A+SSC.A, dfs_fs_t_auto_normfda, main="auto")
 dev.off()
 gc()
 jpeg(file = paste0(figureDirectory,"normaliseddensityplot2.jpeg"))
-densityplot(~GPR32...AF488.A+FoxP3.PE.A, dfs_fs_t_manual_normfda, main="auto")
+densityplot(~GPR32...AF488.A+FoxP3.PE.A, dfs_fs_t_auto_normfda, main="auto")
 dev.off()
 gc()
 jpeg(file = paste0(figureDirectory,"normaliseddensityplot3.jpeg"))
-densityplot(~CD19...PE.CF595.A+IgD...PerCP.Cy5.5.A, dfs_fs_t_manual_normfda, main="auto")
+densityplot(~CD19...PE.CF595.A+IgD...PerCP.Cy5.5.A, dfs_fs_t_auto_normfda, main="auto")
 dev.off()
 gc()
 jpeg(file = paste0(figureDirectory,"normaliseddensityplot4.jpeg"))
-densityplot(~IFNg.PE.Cy7.A+FPRL1...AF647.A, dfs_fs_t_manual_normfda, main="auto")
+densityplot(~IFNg.PE.Cy7.A+FPRL1...AF647.A, dfs_fs_t_auto_normfda, main="auto")
 dev.off()
 gc()
 jpeg(file = paste0(figureDirectory,"normaliseddensityplot5.jpeg"))
-densityplot(~Zombie.NIR.A+IL.17...BV421.A, dfs_fs_t_manual_normfda, main="auto")
+densityplot(~Zombie.NIR.A+IL.17...BV421.A, dfs_fs_t_auto_normfda, main="auto")
 dev.off()
 gc()
 jpeg(file = paste0(figureDirectory,"normaliseddensityplot6.jpeg"))
-densityplot(~CD24...BV605.A+CD27...BV650.A, dfs_fs_t_manual_normfda, main="auto")
+densityplot(~CD24...BV605.A+CD27...BV650.A, dfs_fs_t_auto_normfda, main="auto")
 dev.off()
-gc()
-##############################
-####### EXPORT TO FCS ########
-##############################
-
-## The flowset (dfs_fs_t_manual_normfda) can be exported to individual fcs files
-# Create an 'output' folder
-gc()
-dir.create("dataPPOutput", showWarnings = FALSE)
-gc()
-
-#Save flowframes wihtin flowset as fcs files using the flowCore package
-write.flowSet(dfs_fs_t_manual_normfda, outdir='dataPPOutput', filename = paste0(gsub(".csv", ".fcs", sampleNames(dfs_fs_t_manual_normfda))))
 gc()
